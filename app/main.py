8b2b7477321eb1885f72b4cb69490a5311840b58
jgoraal <jakub.gorski@student.put.poznan.pl>
2025-05-22 19:01:29 +0200

    Set debug mode to False and remove unused Plotly template.

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ from app.init_db import create_dash_app
 app = create_dash_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
