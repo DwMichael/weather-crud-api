06aa011e9707b0a47bef06984ac974a7490ba248
Rabbitv <rabbitv167@gmail.com>
2025-05-22 18:00:24 +0200

    Add Fontend to app

--- app/init_db.py
+++ app/init_db.py
@@ -1,7 +1,11 @@
 # app/init_db.py
+from dash import Dash, dash
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import dash_bootstrap_components as dbc
+from app.callbacks.cards_callback import register_callbacks
+from app.layout.layouts import main_layout
 
 db = SQLAlchemy()
 
@@ -17,5 +21,29 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    return app
+
+
+def create_dash_app():
+    # Tworzymy Flask server osobno
+    server = Flask(__name__)
+    server.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///local.db')
+    server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(server)
+
+    from app.controllers.routes import routes_bp
+    server.register_blueprint(routes_bp)
+
+    with server.app_context():
+        db.create_all()
+
+    # Tworzymy Dash na bazie tego Flaska
+    app = Dash(__name__, server=server, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
+    app.title = "Symulacja zbiornika"
+
+    register_callbacks(app)
+    app.layout = main_layout
 
     return app
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 # app/main.py
 import os
-from app.init_db import create_app
+from app.init_db import create_dash_app
 
-app = create_app()
+app = create_dash_app()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
